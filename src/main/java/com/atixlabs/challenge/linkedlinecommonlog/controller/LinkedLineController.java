05b09fac308d97b93e67b4064eb304cda436147d
beefre <omarvalentin@gmail.com>
2022-04-13 18:08:02 -0500

    Moved to controller package

--- src/main/java/com/atixlabs/challenge/linkedlinecommonlog/controller/LinkedLineController.java
+++ src/main/java/com/atixlabs/challenge/linkedlinecommonlog/controller/LinkedLineController.java
@@ -1,4 +1,4 @@
-package com.atixlabs.challenge.linkedlinecommonlog;
+package com.atixlabs.challenge.linkedlinecommonlog.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
