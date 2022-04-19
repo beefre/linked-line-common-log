250ce0229a6217b2533527741d696006319482f1
beefre <omarvalentin@gmail.com>
2022-04-19 14:35:59 -0500

    Code coverage completed

--- src/test/java/com/atixlabs/challenge/linkedlinecommonlog/LinkedLineControllerMockTest.java
+++ src/test/java/com/atixlabs/challenge/linkedlinecommonlog/LinkedLineControllerMockTest.java
@@ -27,12 +27,6 @@ class LinkedLineControllerMockTest {
 	@InjectMocks
 	LinkedLineController linkedLineController;
 	
-//	@Mock
-//	FileLinkedLineServiceImpl fileLinkedLineService;
-	
-//	@Mock
-//	File file = new File("C:\\demo\\test.csv");
-
 	@Test
 	void testCreate_genesisHash() {
 		FileLinkedLineService fileLinkedLineService = mock(FileLinkedLineService.class);
@@ -45,21 +39,5 @@ class LinkedLineControllerMockTest {
 		when(fileLinkedLineService.getPreviousHash(file)).thenReturn(Constants.GENESIS_PREV_HASH);
 		linkedLineController.create(line);
 	}
-	
-	
-//	@Test
-//	void testCreate() {
-//		FileLinkedLineService fileLinkedLineService = mock(FileLinkedLineService.class);
-//		linkedLineController = new LinkedLineController(fileLinkedLineService);
-//		org.springframework.test.util.ReflectionTestUtils.setField(linkedLineController, "location", "C:\\demo\\test.csv");
-//		File file = new File("C:\\demo\\test.csv");
-//		Line line = new Line("9861e16a52e13657d01d2fe483f1f6337ee92fda1b7b92f668f1e51d5a4b","Hi");
-//		
-//		line.setHash("9913cdf04430415a76477e3cb22cf113138abf77f8dfa4e1bb1264f1c40c0");
-//		boolean bln = false;
-//		when(fileLinkedLineService.createFile(new File("C:\\demo\\test.csv"))).thenReturn(bln);
-//		when(fileLinkedLineService.getPreviousHash(file)).thenReturn("0061e16a52e13657d01d2fe483f1f6337ee92fda1b7b92f668f1e51d5a4b");
-//		linkedLineController.create(line);
-//	}
 
 }
